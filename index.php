c24acd2e456bca2120999d252b6ffdc4ef5a6c25
prakkosal1111@gmail.com <prakkosal1111@gmail.com>
2023-03-08 22:38:30 +0700

    my first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    echo "Hello world"
+    ?>
+</body>
+
+</html>
