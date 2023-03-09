808029c8ff7aeeea7df39a314ac203a8768e1a57
prakkosal1111@gmail.com <prakkosal1111@gmail.com>
2023-03-09 22:54:43 +0700

    lesson 5 associativeArray

--- index.php
+++ index.php
@@ -9,16 +9,34 @@
     <body>
             <?php
             $arrayBook= [
-                " name of the book one",
-                "name of the book two", 
-                "name of the book three",
-                "name of the book four"
+                    [
+                        "name"=>"author name one",
+                        "book"=>"Book name one",
+                        "link"=>"https://www.pdfdrive.com/learning-php-mysql-javascript-css-html5-a-step-by-step-guide-to-creating-dynamic-websites-e158606918.html",
+                    ],
+                    [
+                        "name"=>"author name two",
+                        "book"=>"Book name two",
+                        "link"=>"https://www.pdfdrive.com/learning-php-mysql-javascript-css-html5-a-step-by-step-guide-to-creating-dynamic-websites-e158606918.html",
+                    ],
+                    [
+                        "name"=>"author name three",
+                        "book"=>"Book name three",
+                        "link"=>"https://www.pdfdrive.com/learning-php-mysql-javascript-css-html5-a-step-by-step-guide-to-creating-dynamic-websites-e158606918.html",
+                    ],
+                    [
+                        "name"=>"author name four",
+                        "book"=>"Book name four",
+                        "link"=>"https://www.pdfdrive.com/learning-php-mysql-javascript-css-html5-a-step-by-step-guide-to-creating-dynamic-websites-e158606918.html",
+                    ]
                 ];
             ?>
             <ul>
-                <?php foreach ($arrayBook as $key=> $value) : ?>
+                <?php foreach ($arrayBook as $book) : ?>
                     <li>
-                        <?php echo  ($key+1).", ".$value?>
+                        <a href="<?php echo $book["link"] ?>" target="_blank">
+                        <?php echo  $book["name"] ." ". $book["book"] ?>
+                    </a>
                     </li>
                 <?php endforeach; ?>
             </ul>
